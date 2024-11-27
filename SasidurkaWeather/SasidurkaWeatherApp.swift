920f56a6db1bd068ddc3cde4e1ca097c2564197c
Sasidurka Venkatesan <venkasas@sheridancollege.ca>
2024-11-26 22:02:24 -0500

    Initial Commit

---
+++ SasidurkaWeather/SasidurkaWeatherApp.swift
@@ -0,0 +1,20 @@
+//
+//  SasidurkaWeatherApp.swift
+//  SasidurkaWeather
+//
+//  Created by Sasidurka on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct SasidurkaWeatherApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
